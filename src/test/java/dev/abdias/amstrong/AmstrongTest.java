4e85a6e6b7e34c419a8740c50532beb9d5d02b83
abdias labrador <abdiaslabrador@gmail.com>
2024-11-08 16:17:26 +0100

    testAmstrong class tested

--- src/test/java/dev/abdias/amstrong/AmstrongTest.java
+++ src/test/java/dev/abdias/amstrong/AmstrongTest.java
@@ -4,5 +4,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class AmstrongTest {
-    
+    @Test
+    void testIsAmstrongNumnb() {
+
+        assertEquals(true, Amstrong.isAmstrongNumnb(371), "El número positivo debe coincidir");
+        assertEquals(true, Amstrong.isAmstrongNumnb(1634), "El número positivo debe coincidir");
+    }
+
+    @Test
+    void testIsNotAmstrongNumnb() {
+
+        assertEquals(false, Amstrong.isAmstrongNumnb(351), "El número positivo debe coincidir");
+        assertEquals(false, Amstrong.isAmstrongNumnb(2015), "El número positivo debe coincidir");
+    }
 }
